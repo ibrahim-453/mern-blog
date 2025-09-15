9f68fc5b79979e12af1e3db5ab0b00bbd18f5ac5
ibrahim-453 <ranaibrahim453@gmail.com>
2025-09-15 17:05:02 +0500

    Add routing and page components for the blog application

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <header>
+      <div>
+
+      </div>
+      <nav>
+        
+      </nav>
+    </header>
+  )
+}
+
+export default NavBar
---
+++ frontend/src/layout/Layout.jsx
@@ -0,0 +1,17 @@
+import NavBar from '../components/NavBar'
+import {Outlet} from 'react-router-dom'
+import Footer from '../components/Footer'
+
+function Layout() {
+  return (
+    <div>
+        <NavBar/>
+        <main>
+            <Outlet/>
+        </main>
+        <Footer />
+    </div>
+  )
+}
+
+export default Layout
