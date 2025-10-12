a7f0465b1bddc80dac968f428ec4d7288e6416b5
ibrahim-453 <ranaibrahim453@gmail.com>
2025-10-12 12:26:04 +0500

    refactor: Remove unused server proxy configuration from Vite setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
- server:{
-    proxy:{
-      "/api":{
-        target: "https://mern-blog-backend-theta-self.vercel.app",
-        changeOrigin: true,
-      },
-    },
-  },
   plugins: [
     tailwindcss(),
     react()],
