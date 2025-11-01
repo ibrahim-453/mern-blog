f1671f584d28c093855459f110df6333fe0b01ae
ibrahim-453 <ranaibrahim453@gmail.com>
2025-11-02 01:23:22 +0500

    adding backend url to frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 export default defineConfig({
  server:{
     proxy:{
       "/api":{
-        target: "http://localhost:3000",
+        target: process.env.VITE_BACKEND_URL,
         changeOrigin: true,
       },
     },
