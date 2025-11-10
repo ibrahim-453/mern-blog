b776b27f6a9a33430eaf10a962bc90cdbbaab0b0
ibrahim-453 <ranaibrahim453@gmail.com>
2025-11-11 00:11:08 +0500

    refactor: enhance static file serving and improve path handling

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,6 +7,8 @@ import userRouter from "./routes/user.routes.js";
 import blogRouter from "./routes/blog.routes.js";
 import commentRouter from "./routes/comment.routes.js";
 import contactRouter from "./routes/contact.routes.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 app.use(
   cors({
@@ -14,11 +16,13 @@ app.use(
     credentials: true,
   })
 );
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/user", userRouter);
