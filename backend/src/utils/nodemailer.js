f043aba8b7bddbb27d387cf881e6c9abeb12f8fd
ibrahim-453 <ranaibrahim453@gmail.com>
2025-11-12 23:28:11 +0500

    refactor: configure nodemailer transport settings for improved security

--- backend/src/utils/nodemailer.js
+++ backend/src/utils/nodemailer.js
@@ -1,12 +1,14 @@
-import nodemailer from 'nodemailer'
+import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-    host : "smtp-relay.brevo.com",
-    port : 587,
-    auth : {
-        user : process.env.SMTP_USER,
-        pass : process.env.SMTP_PASS
-    }
-})
-
-export default transporter
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
